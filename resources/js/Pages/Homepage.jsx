ff58d9dda51f69c9c7a0c8b9ac2700e628efd78d
sglavas <stjepanglavas@proton.me>
2026-01-02 12:02:23 +0100

    INITIAL COMMIT

---
+++ resources/js/Pages/Homepage.jsx
@@ -0,0 +1,9 @@
+import Layout from "./Components/Layout"
+
+export default function Homepage() {
+    return(
+        <Layout>
+            <h1>This is the Home Page</h1>
+        </Layout>
+    )
+}
