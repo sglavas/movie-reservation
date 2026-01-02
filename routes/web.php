1bf898776068142c19f036e832fa9db571ad7cf9
sglavas <stjepanglavas@proton.me>
2026-01-02 12:49:34 +0100

    chore: render HomePage without props

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,6 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Homepage', [
-        'name' => 'Simon',
-    ]);
+    return Inertia::render('HomePage');
 });
 
