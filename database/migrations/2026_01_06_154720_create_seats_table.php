88ae03ebfc34b3f8654654d781a12f6f117718c0
sglavas <stjepanglavas@proton.me>
2026-01-10 17:39:57 +0100

    fix(db): add foreign key to Seat migration

--- database/migrations/2026_01_06_154720_create_seats_table.php
+++ database/migrations/2026_01_06_154720_create_seats_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Screen;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,12 +14,12 @@ return new class extends Migration
     {
         Schema::create('seats', function (Blueprint $table) {
             $table->id();
-            $table->integer('screen_id');
+            $table->foreignIdFor(Screen::class)->constrained()->cascadeOnDelete();
             $table->string('row');
             $table->integer('number');
             $table->string('type');
             $table->timestamps();
-        });
+        }); 
     }
 
     /**
