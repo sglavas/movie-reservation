d934af95ce080abb6c1b778e76125339db8db994
sglavas <stjepanglavas@proton.me>
2026-01-10 17:41:36 +0100

    fix(seeder): reorganize seeder calls in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,9 +17,9 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        $this->call(SeatSeeder::class);
-        $this->call(ScreenSeeder::class);
         $this->call(TheaterSeeder::class);
+        $this->call(ScreenSeeder::class);
+        $this->call(SeatSeeder::class);
         $this->call(MovieSeeder::class);
 
         User::factory()->create([
