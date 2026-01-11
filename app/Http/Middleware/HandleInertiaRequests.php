87553058044ac9b53ac840a872119b02805567a3
sglavas <stjepanglavas@proton.me>
2026-01-11 15:42:02 +0100

    feat: share request path using HandleInertiaRequests

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -37,6 +37,7 @@ class HandleInertiaRequests extends Middleware
     {
         return [
             ...parent::share($request),
+            'path' => request()->path(),
             //
         ];
     }
