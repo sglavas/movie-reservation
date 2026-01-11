5a523d2230b475309c173571185e898e88592bec
sglavas <stjepanglavas@proton.me>
2026-01-11 15:46:20 +0100

    feat: access shared data to toggle currently active navlink buttons

--- resources/js/Pages/Components/NavigationLink.jsx
+++ resources/js/Pages/Components/NavigationLink.jsx
@@ -1,23 +1,28 @@
+import { usePage } from "@inertiajs/react";
+
 const navigation = [
   { name: 'Home', href: '/', current: false },
   { name: 'Contact', href: '/contact', current: false },
   { name: 'Movies ', href: '/movies', current: false },
 ]
 
+
 function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
+    return classes.filter(Boolean).join(' ')
 }
 
 export default function NavigationLink() {
+    const {path} = usePage().props;
+
     return(
         <div className="ml-10 flex items-baseline space-x-4">
             {navigation.map((item) => (
                 <a
                     key={item.name}
                     href={item.href}
-                    aria-current={item.current ? 'page' : undefined}
+                    aria-current={item.href === path ? 'page' : undefined}
                     className={classNames(
-                        item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-white/5 hover:text-white',
+                        item.href === path ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-white/5 hover:text-white',
                         'rounded-md px-3 py-2 text-sm font-medium',
                     )}
                 >
