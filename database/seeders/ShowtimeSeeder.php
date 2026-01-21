f9ebc39127a4a0737ffb2cac20a30a5abbf3cce8
sglavas <stjepanglavas@proton.me>
2026-01-21 14:05:53 +0100

    refactor(seeder): Refactor logic in ShowtimeSeeder - Implement ExtractSeedService class instead of csvToArray helper function - Use the Eloquent create() method instead of the Query Builder insert() method to fire off the corresponding event - Wrap the create() method with a DB transaction

--- database/seeders/ShowtimeSeeder.php
+++ database/seeders/ShowtimeSeeder.php
@@ -3,11 +3,19 @@
 namespace Database\Seeders;
 
 use App\Models\Showtime;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Services\Seeder\ExtractSeedService;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class ShowtimeSeeder extends Seeder
 {
+    public function __construct(
+        protected ExtractSeedService $extractionService,
+    )
+    {
+        //
+    }
+
     /**
      * Run the database seeds.
      */
@@ -15,24 +23,15 @@ class ShowtimeSeeder extends Seeder
     {
         $fileName = database_path('seeds/csvs/showtime.csv');
 
-        $data = csvToArray($fileName);
-
-        // If data doesn't exist
-        if(!$data){
-            $this->command->error("Data could not be retrieved");
-            return;
-        }
-
-        // Calculate end time
-        $newData = calcEndTime($data);
-
-        if(!$newData){
-            $this->command->error('Data could not be retrieved');
-            return;
-        }
+        $data = $this->extractionService->extractData($fileName);
 
         $this->command->info("Creating sample showtimes...");
 
-        Showtime::insert($newData);
+        DB::transaction(function () use($data) {
+            foreach($data as $record){
+                Showtime::create($record);
+            }
+        });
+
     }
 }
