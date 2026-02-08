fd1f0d70ace801d41f59154a712e91886b226507
sglavas <stjepanglavas@proton.me>
2026-02-09 00:03:22 +0100

    feat(controller): add is_bookable property to showtime

--- app/Http/Controllers/ShowtimeController.php
+++ app/Http/Controllers/ShowtimeController.php
@@ -6,6 +6,7 @@ use App\Http\Resources\MovieResource;
 use App\Models\Movie;
 use App\Services\Showtime\GroupShowtimeService;
 use App\Services\Showtime\OrderDateService;
+use App\Services\Showtime\SetBookableService;
 use App\Services\Showtime\ShowtimeResourceService;
 use Carbon\Carbon;
 use Inertia\Inertia;
@@ -16,6 +17,7 @@ class ShowtimeController extends Controller
         protected GroupShowtimeService $groupingService,
         protected OrderDateService $orderingService,
         protected ShowtimeResourceService $filteringService,
+        protected SetBookableService $bookableService
     )
     {
         //
@@ -26,6 +28,7 @@ class ShowtimeController extends Controller
         // Fetch the movies together with the corresponding showtimes and map over the array
         $movies = Movie::with('showtimes')->get()->mapWithKeys(function ($movie) {
 
+            // Group showtimes by date and time string
             $groupedShowtimes = $this->groupingService->groupShowtimes($movie);
 
             // Order showtime keys by ascending (oldest first)
@@ -34,10 +37,13 @@ class ShowtimeController extends Controller
             // Filter database data using ShowtimeResource
             $filteredDates = $this->filteringService->useResource($orderedDates);
 
+            // If start time is after now, set is_bookable showtime property to false
+            $bookableDates = $this->bookableService->setBookable($filteredDates);
+
             // Return an array with movie IDs as custom keys and an array of movie information and showtimes
             return [$movie->id => [
                 'movie' => new MovieResource($movie),                           // Filter data using MovieResource
-                'showtimes' => $filteredDates
+                'showtimes' => $bookableDates
             ]];
         });
 
