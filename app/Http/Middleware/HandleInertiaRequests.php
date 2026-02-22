627ccbb56d14a68b8c9278a548af87d7f7942b72
sglavas <stjepanglavas@proton.me>
2026-02-22 13:15:32 +0100

    chore: remove unused path in HandleInertiaRequests

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\Movie;
 use Illuminate\Http\Request;
 use Inertia\Middleware;
 
@@ -37,7 +38,6 @@ class HandleInertiaRequests extends Middleware
     {
         return [
             ...parent::share($request),
-            'path' => request()->path(),
             //
         ];
     }
