21025b14b9a9890fa9b968775c2630a382e8af69
sglavas <stjepanglavas@proton.me>
2026-02-22 15:18:31 +0100

    refactor: move GroupShowtimeService, OrderDateService and SetBookable service logic into ShowtimePipelineService private methods to increase cohesion and decrease coupling

--- app/Services/Showtime/ShowtimePipelineService.php
+++ app/Services/Showtime/ShowtimePipelineService.php
@@ -3,6 +3,8 @@
 namespace App\Services\Showtime;
 
 use App\Models\Movie;
+use Carbon\Carbon;
+use Carbon\CarbonPeriod;
 use Illuminate\Support\Collection;
 
 class ShowtimePipelineService
@@ -16,14 +18,91 @@ class ShowtimePipelineService
      * @param SetBookableService $bookableService Sets the bookable property
      */
     public function __construct(
-        protected GroupShowtimeService $groupingService,
-        protected OrderDateService $orderingService,
         protected ShowtimeResourceService $filteringService,
-        protected SetBookableService $bookableService,
 
     )
     {
         //
+    }
+
+    private function groupShowtimes (Movie $movie): Collection
+    {
+        // Group the showtimes according to date
+        $groupedShowtimes = $movie->showtimes->groupBy(function ($showtime) {
+            $parsedTime = Carbon::parse($showtime['start_time']);
+
+            // Group today's showtimes by "today, Month Day"
+            if($parsedTime->isToday()){
+                // return 'today,' . " " . $parsedTime->format('M d');
+                return 'today';
+            }
+
+            // Group tomorrow's showtimes by "tomorrow, Month Day"
+            if($parsedTime->isTomorrow()){
+                // return 'tomorrow, ' . " " . $parsedTime->format('M d');
+                return 'tomorrow';
+            }
+
+            // Create a period between the first day of the month and the last day of the month
+            $period = CarbonPeriod::between(Carbon::parse($parsedTime)->startOfMonth(), Carbon::parse($parsedTime)->endOfMonth())
+                ->filter(function ($date) {
+                    return $date;
+                });
+            
+            // Loop over the generated period to find the relevant day
+            foreach($period as $day){
+                if(Carbon::parse($day)->isSameDay($parsedTime)){
+                    // Group by date (Day of the week, Month Day)
+                    return $parsedTime->format('D') . ", " . $parsedTime->format('M d');
+                }
+            }
+
+        });
+
+        return $groupedShowtimes;
+
+    }
+
+
+    private function orderDates(Collection $groupedShowtimes): Collection
+    {
+        // Order showtime keys by ascending (oldest first)
+        $orderedDates = $groupedShowtimes->sortKeysUsing(function ($a, $b) {
+            $firstPositionParsed = Carbon::parse($a);
+            $secondPositionParsed = Carbon::parse($b);
+
+            return $firstPositionParsed > $secondPositionParsed;
+        });
+
+        //Sort showtime grid by screen number
+        $sortedByScreen = $orderedDates->map(function($sortedShwotime) {
+            return $sortedShwotime->sortBy('screen_id')->values();
+        });
+
+        // Sort showtime grid by date and time
+        $sortedByTime = $sortedByScreen->map(function($orderedDate) {
+            return $orderedDate->sortBy('start_time')->values();
+        });
+
+
+        return $sortedByTime;
+    }
+
+
+    private function setBookable(Collection $filteredDates): Collection
+    {
+        $bookableDates =  $filteredDates->map(function($filteredDate) {
+            $bookableDate =  $filteredDate->map(function($showtime) {
+
+                // If the start time of a showtime is after the current date and time, set is_bookable to false
+                $showtimeCollection = collect($showtime)->put('is_bookable', !Carbon::now()->isAfter($showtime->start_time));
+                return $showtimeCollection;
+
+            });
+            return $bookableDate;
+        });
+
+        return $bookableDates;
     }
 
     /**
@@ -35,16 +114,16 @@ class ShowtimePipelineService
     public function usePipeline(Movie $movie): Collection
     {
             // Group showtimes by date and time string
-            $groupedShowtimes = $this->groupingService->groupShowtimes($movie);
+            $groupedShowtimes = $this->groupShowtimes($movie);
 
             // Order showtime keys by ascending (oldest first)
-            $orderedDates = $this->orderingService->orderDates($groupedShowtimes);
+            $orderedDates = $this->orderDates($groupedShowtimes);
 
             // Filter database data using ShowtimeResource
             $filteredDates = $this->filteringService->useResource($orderedDates);
 
             // If start time is after now, set is_bookable showtime property to false
-            $bookableDates = $this->bookableService->setBookable($filteredDates);
+            $bookableDates = $this->setBookable($filteredDates);
 
             return $bookableDates;
 
