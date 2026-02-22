fae726dbdb53c5e948834f5b0780c94f564cc742
sglavas <stjepanglavas@proton.me>
2026-02-22 13:18:34 +0100

    refactor: Move end time calculation logic from ShowtimeObeserver to CalculateEndTimeService class

--- app/Observers/ShowtimeObserver.php
+++ app/Observers/ShowtimeObserver.php
@@ -3,33 +3,33 @@
 namespace App\Observers;
 
 use App\Models\Showtime;
-use Carbon\Carbon;
+use App\Services\Showtime\CalculateEndTimeService;
 
 class ShowtimeObserver
 {
+    public function __construct(
+        protected CalculateEndTimeService $calculatingService
+    )
+    {
+        //
+    }
     /**
      * Handle the Showtime "creating" event.
      * 
      * Calculates the end time of a movie.
      * End time is calculated by adding the movie duration and a 30-minute buffer to the showtime's start_time.
-     * 
+     * Mutate the original object by adding the calculated end time
      *
      * @param Showtime $showtime A showtime model
      * @return void
      */
     public function creating(Showtime $showtime): void
     {
-        if(empty($showtime->end_time)){
-            // Get the movie this showtime belongs to
-            $movie = $showtime->movie;
-    
-            // Add 15 minutes before and after each movie as buffer
-            $timeToAdd = $movie->duration + 30;
+        // Get the Showtime with the calculated end time
+        $calculatedEndTime = $this->calculatingService->calculateEndTime($showtime);
 
-            $endTime = Carbon::parse($showtime->start_time)->addMinutes($timeToAdd);
-    
-            $showtime->end_time = $endTime;
-        }
+        // Mutate the original Showtime model
+        $showtime->end_time = $calculatedEndTime->end_time;
 
     }
 
---
+++ app/Services/Showtime/CalculateEndTimeService.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Services\Showtime;
+
+use App\Models\Showtime;
+use Carbon\Carbon;
+
+class CalculateEndTimeService
+{
+    /**
+     * Calculates the end time of a showtime
+     *
+     * @param Showtime $showtime An in-memory Showtime model instance.
+     * @return Showtime The model updated with the calculated end_time.
+     */
+    public function calculateEndTime(Showtime $showtime): Showtime
+    {
+        if(empty($showtime->end_time)){
+            
+            $calculatedEndTime = clone $showtime;
+
+            // Get the movie this showtime belongs to
+            $movie = $calculatedEndTime->movie;
+
+            // Add a 30-minute buffer (15 mins for cleaning, 15 for trailers)
+            $timeToAdd = $movie->duration + 30;
+
+            $endTime = Carbon::parse($calculatedEndTime->start_time)->addMinutes($timeToAdd);
+
+            $calculatedEndTime->end_time = $endTime;
+        }
+        return $calculatedEndTime;
+    }
+}
