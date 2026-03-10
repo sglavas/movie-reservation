650cbba6a1de90f012c3ef9866223fe87d767677
sglavas <stjepanglavas@proton.me>
2026-03-10 23:41:25 +0100

    feat: create theater factory

---
+++ database/factories/TheaterFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Theater>
+ */
+class TheaterFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->company(),
+            'city' => fake()->city(),
+            'total_screens' => 5,
+            'seats' => 500
+        ];
+    }
+}
