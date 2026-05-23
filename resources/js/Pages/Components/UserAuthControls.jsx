450af8bead53125308f5c95c98e1a73a650ef01b
sglavas <stjepanglavas@proton.me>
2026-05-23 08:07:46 +0200

    feat(UI): add controls for user authentication

---
+++ resources/js/Pages/Components/UserAuthControls.jsx
@@ -0,0 +1,33 @@
+import { useForm, usePage } from "@inertiajs/react";
+import NavigationLink from "./NavigationLink";
+import Button from "./Button";
+
+export default function UserAuthControls() {
+    const { auth } = usePage().props;
+    const { post, processing } = useForm();
+
+    const submit = (e) => {
+        e.preventDefault();
+        post('/logout');
+    };
+
+    if(!auth.user){
+        return(
+            <NavigationLink 
+                navigation={[ {name: 'Log In', href: '/login'}, {name: 'Register', href: '/register'} ]}
+            />
+        );
+    }else{
+        return(
+            <form onSubmit={submit} action="/logout">
+                <Button as='button'
+                        color='indigo'
+                        type='submit'
+                        disabled={processing}
+                >
+                    Log Out
+                </Button>
+            </form>
+        )
+    }
+}
