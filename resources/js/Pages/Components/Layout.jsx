4fd48be282279f3a2415ad36160a0e955873331b
sglavas <stjepanglavas@proton.me>
2026-05-23 08:32:07 +0200

    refactor(layout): migrate navigation link into separate component and import component

--- resources/js/Pages/Components/Layout.jsx
+++ resources/js/Pages/Components/Layout.jsx
@@ -3,6 +3,7 @@ import { BellIcon } from '@heroicons/react/24/outline'
 import NavigationLink from './NavigationLink'
 import { Link } from '@inertiajs/react'
 import Button from './Button'
+import UserAuthControls from './UserAuthControls'
 
 const user = {
   name: 'Tom Cook',
@@ -43,44 +44,8 @@ export default function Layout(props) {
                 </div>
               </div>
               <div className="hidden md:block">
-                <div className="ml-4 flex items-center md:ml-6">
-                  <button
-                    type="button"
-                    className="relative rounded-full p-1 text-gray-400 hover:text-white focus:outline focus:outline-2 focus:outline-offset-2 focus:outline-indigo-500"
-                  >
-                    <span className="absolute -inset-1.5" />
-                    <span className="sr-only">View notifications</span>
-                    <BellIcon aria-hidden="true" className="size-6" />
-                  </button>
-
-                  {/* Profile dropdown */}
-                  <Menu as="div" className="relative ml-3">
-                    <MenuButton className="relative flex max-w-xs items-center rounded-full focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">
-                      <span className="absolute -inset-1.5" />
-                      <span className="sr-only">Open user menu</span>
-                      <img
-                        alt=""
-                        src={user.imageUrl}
-                        className="size-8 rounded-full outline outline-1 -outline-offset-1 outline-white/10"
-                      />
-                    </MenuButton>
-
-                    <MenuItems
-                      transition
-                      className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg outline outline-1 outline-black/5 transition data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
-                    >
-                      {userNavigation.map((item) => (
-                        <MenuItem key={item.name}>
-                          <a
-                            href={item.href}
-                            className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:outline-none"
-                          >
-                            {item.name}
-                          </a>
-                        </MenuItem>
-                      ))}
-                    </MenuItems>
-                  </Menu>
+                <div>
+                  <UserAuthControls />
                 </div>
               </div>
 
@@ -106,6 +71,6 @@ export default function Layout(props) {
           <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">{props.children}</div>
         </main>
       </div>
-    </>
+    </div>
   )
 }
